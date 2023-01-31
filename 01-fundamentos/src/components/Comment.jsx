c505cb2068ec9b6fd8ec1419786bdb9e83e4aee5
Gabriel Saturnino <gabrielsaturnino4@gmail.com>
2023-01-31 18:36:17 -0300

    componente comentario

---
+++ 01-fundamentos/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import style from './Comment.module.css'
+import { ThumbsUp, Trash } from "phosphor-react";
+
+export function Comment() {
+  return (
+    <div className={style.comment}>
+      <img src='https://avatars.githubusercontent.com/u/80297011?v=4' alt='' />
+
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Autor do comment</strong>
+              <time title='20 de Janeiro' dateTime='2023-01-31 17:41:38'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom, Parabens</p>
+
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
