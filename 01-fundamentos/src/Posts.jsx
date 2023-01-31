f8609996debd74ecc6968b528310f51ad7f64e69
Gabriel Saturnino <gabrielsaturnino4@gmail.com>
2023-01-31 17:47:42 -0300

    componente de post

--- 01-fundamentos/src/Posts.jsx
+++
@@ -1,9 +0,0 @@
-export function Post({ autor, content }) {
-  return (
-    <>
-      <strong> Autor: {autor} </strong>
-      <span>content: {content}</span>
-    </>
-  )
-}
-
---
+++ 01-fundamentos/src/components/Post.jsx
@@ -0,0 +1,28 @@
+import style from './post.module.css'
+
+export function Post() {
+  return (
+    <article className={style.post}>
+      <header>
+        <div className={style.author}>
+          <img
+            className={style.avatar}
+            src='https://avatars.githubusercontent.com/u/80297011?v=4' />
+          <div className={style.authorInfo}>
+            <strong>Gabriel S.</strong>
+            <span>conteudo aqui</span>
+          </div>
+        </div>
+
+        <time title='20 de Janeiro' dateTime='2023-01-31 17:41:38'>Publicado há 1h</time>
+      </header>
+
+      <div className={style.content}>
+        <p>Fala galeraa 👋
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>Sequi temporibus tempore quam animi minima dicta necessitatibus nemo, voluptatum aliquid ut eligendi quidem in porro vel debitis dignissimos exercitationem libero ratione?</p>
+        </p>
+      </div>
+    </article>
+  )
+}
